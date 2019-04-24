22f2270fbdefdef2a9210c9d7e5d81779233ffea
Reinaldo J <rejamen@gmail.com>
2019-04-24 15:46:27 -0400

    Counting pairs done

--- counting_pairs.py
+++ counting_pairs.py
@@ -1,1 +1,19 @@
-"""count number of pair element in array """
+"""Counting pairs of numbers in one array."""
+
+ar = [1, 2, 1, 1, 2, 2, 3, 3, 8, 9, 9, 9, 9]
+
+
+def count_pairs(ar):
+	"""Count total pairs of values y ar."""
+	checked_values = []
+	res = 0
+	for value in ar:
+		if value not in checked_values:
+			checked_values.append(value)
+			count = ar.count(value)
+			if count >= 2:
+				res += count / 2
+	return res
+
+print ar
+print count_pairs(ar)
