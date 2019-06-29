4da58e47239dc31b7bd49329c03ba4eee86c3409
Reinaldo Javier Menendez <rejamen@gmail.com>
2019-06-29 10:25:26 -0400

    Reverse args with lambda

--- reverse_arguments.py
+++ reverse_arguments.py
@@ -4,17 +4,16 @@ Given an arbitrary function, return a new function which,
 when called, returns the result of the original function
 called with arguments in reverse order.
 """
-import inspect
-from inspect import signature
 
 
-def original_function(a=2, b=3):
-    """Get args a, b and do something."""
+def original_function(a, b):
+    """Function to be called with reversed args."""
     return pow(a, b)
 
 
-sig = inspect.getfullargspec(original_function)
-print(sig.args)
-print(sig.defaults)
-reverse = sig.defaults.reverse()
-print (reverse)
+def reversed_args(original_function):
+    """Call orignal_function with reversed args."""
+    return lambda *args: original_function(*args[::-1])
+
+g = reversed_args(original_function)
+print(g(2, 3))
